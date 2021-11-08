5c9c1b5442c64b5e9cdfd82743d63426fe63da3b
ursula_vallejo <ursulavallejo@gmail.com>
2021-11-08 13:30:47 +0100

    funcion savebutton

--- uts-main/contacts/js/script.js
+++ uts-main/contacts/js/script.js
@@ -10,6 +10,18 @@ function addContact(){
     updateContactList(contactArray)
 }
 
+function addContactEdit(){
+    let userNameEdit =  document.getElementById('nameInput2').value
+    let userNumberEdit  =  document.getElementById('numberInput2').value
+    let contact = {
+        id:contactArray.length + 1,
+        name: userNameEdit,
+        number: userNumberEdit,
+    }
+    contactArray.push(contact)
+    updateContactList(contactArray)
+}
+
 function deleteContact(id){
     for (let i = 0; i < contactArray.length; i++) {
         if (contactArray[i].id === id){
--- uts-main/contacts/js/templates/contact.js
+++ uts-main/contacts/js/templates/contact.js
@@ -1,16 +1,16 @@
-function updateListInfo(contact){
+function updateListInfo(contact) {
     return `
     <li id="contact-${contact.id}">
-        ${ contact.name}   ${ contact.number}
+        ${contact.name}   ${contact.number}
         <button id="edit-1" onclick="contactEditable()">Edit</button>
-        <button id="delete-${contact.id}" onclick="deleteContact (${ contact.id })">X</button>
+        <button id="delete-${contact.id}" onclick="deleteContact (${contact.id})">X</button>
     </li>
     `
 }
 
-function updateContactList(contactArray){
- let ListContact = '<ul>';
-    for (const item of contactArray){
+function updateContactList(contactArray) {
+    let ListContact = '<ul>';
+    for (const item of contactArray) {
         ListContact += updateListInfo(item)
     }
     ListContact += '<ul>'
@@ -18,19 +18,30 @@ function updateContactList(contactArray){
 }
 
 
-function updateElementIdHtml(elementID, htmlString){
+function updateElementIdHtml(elementID, htmlString) {
     document.getElementById(elementID).innerHTML = htmlString
 }
 
 
+function ItemContactEditable(id) {
+    let nameEdited= document.getElementById('nameInput2' + id).value
+    let numberEdited =  document.getElementById('numberInput2' + id).value
+    for (const item of contactArray) {
+      if (item.id === id){
+          item.name = nameEdited
+          item.number = numberEdited
+      }
+    }
+    updateContactList(contactArray)
+}
 
 function contactEditable(contact){
-    return  `
-    <li id="contact-${contact.id}">
-        <input type="text" id="nameInput" value="${ contact.name}" />
-        <input type="text" id="numberInput" value="${ contact.number}" />
-        <button id="edit-2" onclick=" updateContactList(contactArray)">Save</button>
-    </li>
+    return `
+      <li id="contact-${contact.id}">
+        <input type="text" id="nameInput2" value="${contact.name}" />
+        <input type="text" id="numberInput2" value="${contact.number}" />
+        <button id="edit-2" onclick=" ItemContactEditable(id)">Save</button>
+      </li>
      `
 }
 
